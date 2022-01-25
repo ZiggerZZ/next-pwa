c41031806b302390b103cb087001e57ce6ada47c
Zigfrid Zvezdin <ziggerzz@gmail.com>
2022-01-25 16:10:53 +0100

    withPWA

--- next.config.js
+++ next.config.js
@@ -1,38 +1,5 @@
-// next.config.js
+const withPWA = require('next-pwa')
 
-const withPlugins = require("next-compose-plugins");
-const withPWA = require("next-pwa");
-const path = require("path");
-
-
-const plugins = [
-  [
-    withPWA,
-    {
-      async redirects() {
-        return [
-          {
-            source: "/",
-            destination: "/", // Matched parameters can be used in the destination
-            permanent: false,
-          },
-        ];
-      },
-      env: {
-        BLABLA:
-          "Hello world",
-      },
-      pwa: {
-        dest: "public",
-      },
-      eslint: {
-        // Warning: Dangerously allow production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: false,
-      },
-    },
-  ],
-  /* ...other plugins... */
-];
-
-module.exports = withPlugins(plugins, {});
+module.exports = withPWA({
+  // other next config
+})
